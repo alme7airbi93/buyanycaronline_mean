a18aebb2e3daa5862388b21ab5db144f78d53473
alme7airbi <alme7airbi93@live.com>
2019-08-27 20:42:02 +0400

    init

--- public/chat/config/chat.client.routes.js
+++
@@ -1,8 +0,0 @@
-angular.module('chat').config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-    when('/chat', {
-      templateUrl: 'chat/views/chat.client.view.html'
-    });
-  }
-]);
